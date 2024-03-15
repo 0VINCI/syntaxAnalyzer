3800d5cc3eef6162f5c9cd203ae03c531ef6343f
0VINCI <101973216+0VINCI@users.noreply.github.com>
2024-03-16 00:08:25 +0100

    first commit

---
+++ LM4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+// Definicja wzorca wyrażenia regularnego
+string pattern = @"[0-9]+([+*-/^][0-9]+)+(;[0-9]+([+*-/^][0-9]+)+)*";
+
+// Utworzenie obiektu Regex
+Regex regex = new Regex(pattern);
+
+Console.WriteLine("Wprowadz wyrażenie arytmetyczne do walidacji (lub wpisz 'q' aby zakończyc):");
+
+while (true)
+{
+    Console.Write("Wprowadz wyrazenie: ");
+    string? userInput = Console.ReadLine();
+
+    // Zakończenie programu, jeśli użytkownik wpisze 'q'
+    if (userInput?.ToLower() == "q")
+    {
+        break;
+    }
+
+    // Walidacja wprowadzonego ciągu znaków
+    if (userInput != null && regex.IsMatch(userInput))
+    {
+        Console.WriteLine("Wprowadzone wyrażenie: " + userInput + " jest zgodne z gramatyką");
+    }
+    else
+    {
+        Console.WriteLine("Wprowadzone wyrażenie: " + userInput + " nie jest zgodne z gramatyką");
+    }
+}
+
+Console.WriteLine("Wykonanie programu zostało zakończone");
